24876536f60f9622934f13463177645a2fe805c5
makersmecca <ayudhnandi@gmail.com>
2026-02-08 03:15:22 +0530

    Updated UI and fixed alignments

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -5,8 +5,14 @@ const Landing = ({ setCurrentPage, maximize }) => {
       <button className="pixel-btn" onClick={() => setCurrentPage("message")}>
         You have a new Message!!
       </button>
-      <div className="cat-gif">
-        <img src="/images/cat-heart2.gif" height={100} width={120} />
+      <div className={`${maximize ? "max-cat-gif" : "cat-gif"}`}>
+        <img
+          className="landing-cat"
+          src="/images/cat-heart2.gif"
+          height={100}
+          width={120}
+          onClick={() => setCurrentPage("message")}
+        />
       </div>
     </div>
   );
--- src/Message.jsx
+++ src/Message.jsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
+import NoBtnContent from "./NoBtnContent";
 const Message = () => {
   const [loader, setLoader] = useState(true);
+  const [button, setButton] = useState("");
 
   setTimeout(() => {
     setLoader(false);
-  }, 3500);
+  }, 1000);
+
+  const handleYes = () => {
+    setButton("yes");
+  };
+  const handleNo = () => {
+    setButton("no");
+  };
 
   return (
-    <>
+    <div
+      className={`${button === "" ? "msg-comp" : button === "yes" ? "msg-comp-yes" : "msg-comp-no"}`}
+    >
       {loader ? (
         <div className="loader">
           <div className="loader-text">Loading</div>
@@ -16,9 +27,44 @@ const Message = () => {
           </div>
         </div>
       ) : (
-        <div className="message">Will you be my Valentine?</div>
+        <>
+          {button === "" ? (
+            <>
+              <div className="message">Will you be my Valentine?</div>
+              <div className="option-btns">
+                <img
+                  className="yes-btn"
+                  onClick={handleYes}
+                  src="/images/YesBtn.png"
+                  height={80}
+                  width={80}
+                />
+                <img
+                  className="no-btn"
+                  onClick={handleNo}
+                  src="/images/NoBtn.png"
+                  height={80}
+                  width={80}
+                />
+              </div>
+            </>
+          ) : button === "yes" ? (
+            <div className="yesMessage">
+              <span className="yes-text">Yaaayyy!!!!</span>
+              <div>
+                <img className="confetti-gif" src="/images/confetti.gif" />
+              </div>
+              <div className="yes-gifs">
+                <img src="/images/cat_heart.gif" height={150} width={150} />
+                <img src="/images/idc.gif" height={150} width={150} />
+              </div>
+            </div>
+          ) : (
+            <NoBtnContent />
+          )}
+        </>
       )}
-    </>
+    </div>
   );
 };
 export default Message;
